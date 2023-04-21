48e53c6c8ce92205c821e3a26507f0fd8a178b5d
Skira01 <jeremrichard31620@gmail.com>
2023-04-21 14:54:25 +0200

    Debut

---
+++ test/welcome/WelcomeTest.java
@@ -0,0 +1,14 @@
+package welcome;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
