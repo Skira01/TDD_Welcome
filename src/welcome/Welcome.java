e43d9614305ebedaa5563e60cb79f1ab758e1bc2
Skira01 <jeremrichard31620@gmail.com>
2023-05-04 16:17:44 +0200

    EX_7

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -1,5 +1,8 @@
 package welcome;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Welcome {
 
 	public static String welcome(String nom) {
@@ -7,35 +10,55 @@ public class Welcome {
 	        return "Hello, my friend";
 	    }
 	    String[] sousChaines = nom.split(",");
-	    StringBuilder msg = new StringBuilder();
-	    StringBuilder capitalizedMsg = new StringBuilder();
-	    boolean hasUppercaseName = false;
-	    boolean isFirstLowercaseName = true;
-	    for (int i = 0; i < sousChaines.length; i++) {
-	        String nomCapitalized = sousChaines[i].trim().substring(0, 1).toUpperCase() + sousChaines[i].trim().substring(1);
-	        if (sousChaines[i].equals(sousChaines[i].toUpperCase())) {
-	            hasUppercaseName = true;
-	            capitalizedMsg.append("HELLO, ");
-	            capitalizedMsg.append(nomCapitalized);
-	            capitalizedMsg.append(" !");
+	    List<String> lowercaseNames = new ArrayList<>();
+	    List<String> uppercaseNames = new ArrayList<>();
+	    for (String s : sousChaines) {
+	        s = s.trim();
+	        if (s.equals(s.toUpperCase())) {
+	            uppercaseNames.add(s);
 	        } else {
-	            if (isFirstLowercaseName) {
-	                msg.append("Hello, ");
-	                isFirstLowercaseName = false;
-	            }
-	            msg.append(nomCapitalized);
-	            if (i != sousChaines.length - 1) {
-	                msg.append(", ");
+	            lowercaseNames.add(s);
+	        }
+	    }
+	    StringBuilder msg = new StringBuilder();
+	    if (!lowercaseNames.isEmpty()) {
+	        msg.append("Hello, ");
+	        for (int i = 0; i < lowercaseNames.size(); i++) {
+	            String name = lowercaseNames.get(i).substring(0, 1).toUpperCase() + lowercaseNames.get(i).substring(1);
+	            if (i == lowercaseNames.size() - 1) {
+	                if (lowercaseNames.size() == 1) {
+	                    msg.append(name);
+	                } else {
+	                    msg.append("and ");
+	                    msg.append(name);
+	                }
+	            } else {
+	                msg.append(name);
+	                if (i < lowercaseNames.size() - 2) {
+	                    msg.append(", ");
+	                } else if (i == lowercaseNames.size() - 2) {
+	                    msg.append(" ");
+	                }
 	            }
 	        }
 	    }
-	    if (hasUppercaseName) {
+	    if (!uppercaseNames.isEmpty()) {
 	        if (msg.length() > 0) {
-	            msg.append(". AND ");
+	        	msg.append(". ");
+	            msg.append("AND HELLO, ");
+	        } else {
+	            msg.append("HELLO, ");
 	        }
-	        msg.append(capitalizedMsg);
+	        for (int i = 0; i < uppercaseNames.size(); i++) {
+	            String name = uppercaseNames.get(i).substring(0, 1).toUpperCase() + uppercaseNames.get(i).substring(1);
+	            msg.append(name.toUpperCase());
+	            if (i < uppercaseNames.size() - 1) {
+	                msg.append(" AND ");
+	            } else {
+	                msg.append("!");
+	            }
+	        }
 	    }
 	    return msg.toString();
 	}
- 
 }
--- test/welcome/WelcomeTest.java
+++ test/welcome/WelcomeTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
-
+	
 	@Test
 	void test1() {
 		assertEquals(Welcome.welcome("bob"),"Hello, Bob");
@@ -17,32 +17,40 @@ class WelcomeTest {
 		assertEquals(Welcome.welcome("    "),"Hello, my friend");
 		assertEquals(Welcome.welcome(null),"Hello, my friend");
 	}
-	 
+	
 	@Test
 	void test3() { 
-		assertEquals(Welcome.welcome("JERRY"),"HELLO, JERRY !");
+		assertEquals(Welcome.welcome("JERRY"),"HELLO, JERRY!");
 	}
+	
 	
 	@Test
 	void test4() {
-		assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy, Bob");
-		assertEquals(Welcome.welcome("jon,bob"),"Hello, Jon, Bob");
-		assertNotEquals(Welcome.welcome("jon,bob"),"Hello, jon, bob");
+		assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy and Bob");
+		assertEquals(Welcome.welcome("jon,bob"),"Hello, Jon and Bob");
+		assertNotEquals(Welcome.welcome("jon,bob"),"Hello, jon and bob");
 	} 
 	
 	@Test
 	void test5() {
-		assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy, Bob");
-		assertEquals(Welcome.welcome("amy,jon,bob"),"Hello, Amy, Jon, Bob");
+		assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy and Bob");
+		assertEquals(Welcome.welcome("amy,jon,bob"),"Hello, Amy, Jon and Bob");
 		assertNotEquals(Welcome.welcome("amy,jon,bob"),"Hello, amy, jon, bob");
 	}
 	
 	@Test
 	void test6() {
-		assertEquals(Welcome.welcome("amy,BOB,jerry"),"Hello, Amy, Jerry. AND HELLO, BOB !");
-		assertEquals(Welcome.welcome("amy,jerry"),"Hello, Amy, Jerry");
-		assertEquals(Welcome.welcome("BOB,jerry"),"Hello, Jerry. AND HELLO, BOB !");
+		assertEquals(Welcome.welcome("amy,BOB,jerry"),"Hello, Amy and Jerry. AND HELLO, BOB!");
+		assertEquals(Welcome.welcome("amy,jerry"),"Hello, Amy and Jerry");
+		assertEquals(Welcome.welcome("BOB,jerry"),"Hello, Jerry. AND HELLO, BOB!");
 	}
 	
+	@Test
+	void test7(){
+		assertEquals(Welcome.welcome("amy,jerry"),"Hello, Amy and Jerry");
+		assertEquals(Welcome.welcome("amy,jerry,bob,frank"),"Hello, Amy, Jerry, Bob and Frank");
+		assertEquals(Welcome.welcome("amy,BOB,jerry,JACK"),"Hello, Amy and Jerry. AND HELLO, BOB AND JACK!");
+		assertEquals(Welcome.welcome("BOB,JACK"),"HELLO, BOB AND JACK!");
+	}
 
 }
